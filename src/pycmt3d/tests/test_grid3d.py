74826078dd40cfcdfa968901df8eea113516b6c6
Lucas Sawade <lsawade@princeton.edu>
2020-01-22 21:39:41 -0500

    Bootstrap gridsearch working, and all tests fixed.

--- src/pycmt3d/tests/test_grid3d.py
+++ src/pycmt3d/tests/test_grid3d.py
@@ -17,7 +17,7 @@ import os
 import pytest
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')  # NOQA
-plt.switch_backend('MacOSx')  # NOQA
+# plt.switch_backend('MacOSx')  # NOQA
 from pycmt3d import CMTSource
 from pycmt3d import DataContainer
 from pycmt3d import WeightConfig
