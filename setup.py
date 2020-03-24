2d13c1c1729d82ea1ec7f35835999b3ce3b13989
Lucas Sawade <lsawade@princeton.edu>
2020-03-24 18:00:41 -0400

    add jobutil to requirements

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     install_requires=[
         "obspy>=1.0.0", "numpy", "future>=0.14.1", "flake8",
         "scipy", "spaceweight", "cartopy", "pyshp", "geographiclib",
-        "owslib", "lxml", "psutil"
+        "owslib", "lxml", "psutil", "joblib"
     ],
     extras_require={
         "docs": ["sphinx"]
