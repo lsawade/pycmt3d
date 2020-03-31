1085d24d9041e31e52b8984849e2ed93ce619b5c
Lucas Sawade <lsawade@princeton.edu>
2020-03-30 21:34:50 -0400

    Added color to the logger

--- src/pycmt3d/__init__.py
+++ src/pycmt3d/__init__.py
@@ -2,35 +2,71 @@ from __future__ import (absolute_import, print_function, division)
 import logging
 
 class CustomFormatter(logging.Formatter):
-    """Logging Formatter to add colors and count warning / errors"""
+    """
+    Logging Formatter to add colors and count warning / errors
+
+    This class organizes the customization of the logging output.
+    The formatter as of now outputs the logs in the follwoing manner in
+    order of Loglevel:
+
+    .. code-block:: python
+
+        [2020-03-30 21:00:17] matpy -- [  DEBUG   ] Test Verbose Level (matrixmultiplication.py:53)
+        [2020-03-30 21:00:17] matpy -- [ VERBOSE  ] Test Verbose Level
+        [2020-03-30 21:00:17] matpy -- [  INFO    ] Initializing matrices...
+        [2020-03-30 21:00:17] matpy -- [ WARNING  ] Matrix size exceeds 4 elements.
+        [2020-03-30 21:00:17] matpy -- [  ERROR   ] Test Error Level (matrixmultiplication.py:54)
+        [2020-03-30 21:00:17] matpy -- [ CRITICAL ] Test Critical Level (matrixmultiplication.py:55)
+
+
+    These outputs are colored in the actual output but the formatting is just
+    as shown above. VERBOSE is an extra added LogLevel formatting. More can be
+    added below the comment `EXTRA LEVELS` in the same way the VERBOSE is added.
+
+    The variable VERBOSE is given at the top of the module. That way it can be
+    changed for all depending function
+
+    """
 
     # Setting up the different ANSI color escape sequences for color terminals:
     # Codes are 3/4-bit codes from here:
     # https://en.wikipedia.org/wiki/ANSI_escape_code
     # For 8-colors: use "\x1b[38;5;<n>m" where <n> is the number of the color.
-    # See lightblue
     grey = "\x1b[38;21m"
-    cyan = "\x1b[38;5;81m"  # "\x1b[96;21m"
+    green = "\x1b[38;5;64m"
+    light_grey = "\x1b[38;5;240m"
+    dark_blue = "\x1b[38;5;25m"
+    light_blue = "\x1b[38;5;69m"
     yellow = "\x1b[33;21m"
     red = "\x1b[31;21m"
+    dark_red = "\x1b[38;5;97m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
 
     # Formats The spaces accommodate the different length of the words and
     # amount of detail wanted in the message:
-    format_inf = "[%(asctime)s] %(name)s | %(levelname)s     | %(message)s"
-    format_war = "[%(asctime)s] %(name)s | %(levelname)s  | %(message)s"
-    format_dbg = "[%(asctime)s] %(name)s | %(levelname)s    | %(message)s (%(filename)s:%(lineno)d)"
-    format_err = "[%(asctime)s] %(name)s | %(levelname)s    | %(message)s (%(filename)s:%(lineno)d)"
-    format_cri = "[%(asctime)s] %(name)s | %(levelname)s | %(message)s (%(filename)s:%(lineno)d)"
+    time_fmt = light_grey + "[%(asctime)s]" + reset
+    name_fmt = "%(name)s --"
+    level_fmt = "[" + grey + "%(levelname)s" + reset + "]"
+    pre_fmt = time_fmt + " " + name_fmt + " "
+
+    debug_fmt = "[" + light_blue + "  %(levelname)s   " + reset + "]" \
+        + light_blue + " %(message)s (%(filename)s:%(lineno)d)" + reset
+    info_fmt = "[  %(levelname)s    ] %(message)s"
+    warning_fmt = "[" + yellow + " %(levelname)s  " + reset + "]" \
+                 + yellow + " %(message)s" + reset
+    error_fmt = "[" + red + "  %(levelname)s   " + reset + "]" \
+        + red + " %(message)s (%(filename)s:%(lineno)d)" + reset
+    critical_fmt = "[" + bold_red + " %(levelname)s " + reset + "]" \
+        + bold_red + " %(message)s (%(filename)s:%(lineno)d)" + reset
 
     # Create format dictionary
     FORMATS = {
-        logging.INFO: format_inf,
-        logging.DEBUG: cyan + format_dbg + reset,
-        logging.WARNING: yellow + format_war + reset,
-        logging.ERROR: red + format_err + reset,
-        logging.CRITICAL: bold_red + format_cri + reset
+        logging.DEBUG: pre_fmt + debug_fmt,
+        logging.INFO: pre_fmt + info_fmt,
+        logging.WARNING: pre_fmt + warning_fmt,
+        logging.ERROR: pre_fmt + error_fmt,
+        logging.CRITICAL: pre_fmt + critical_fmt
     }
 
     # Initialize with a default logging.Formatter
@@ -47,6 +83,7 @@ class CustomFormatter(logging.Formatter):
 
         # Return
         return formatter.format(record)
+
 
 # setup the logger
 logger = logging.getLogger("pycmt3d")
